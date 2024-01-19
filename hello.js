1e2d84b9f08502d37b5b18d7d36f0e4bb40ed058
Doss Stone <Doss.s.stl@gmail.com>
2024-01-19 05:52:46 -0600

    changes

--- hello.js
+++ hello.js
@@ -1,22 +1,7 @@
-/*  
-Welcome, new coder. 
-Enter your code on line 7 to print 'Hello, World!' 
-to the console (the panel on the right)
-*/
 
-console.log('"6+6=12" said the frog.')
+const input = require('readline-sync');
 
-/*  
-Next, PLAY! 
-Modify your code on line 7 to try to accomplish the tasks listed below:
+let num1 = (Number(input.question("Enter a number: ")));
+let num2 = (Number(input.question("Enter another number: ")));
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
-   9. Other. You choose!
-*/
+console.log(num1 + num2);
